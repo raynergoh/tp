95b033e13c9e1caa54095139ac14910dd702c494
ron <ronkoh2511@gmail.com>
2025-10-09 22:30:01 +0800

    remove whitespaces

--- src/test/java/seedu/address/model/person/RoleStatusPredicateTest.java
+++ src/test/java/seedu/address/model/person/RoleStatusPredicateTest.java
@@ -1,6 +1,5 @@
 package seedu.address.model.person;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Arrays;
@@ -16,7 +15,7 @@ class RoleStatusPredicateTest {
     void test_matchesRoleOnly_returnsTrue() {
         // TODO: Implement test once RoleStatusPredicate.test() is implemented
         // TODO: Implement PersonBuilder.withRole(String) method
-        Person person = new PersonBuilder() /* .withRole("buyer") */ .build();
+        Person person = new PersonBuilder()/* .withRole("buyer") */.build();
         RoleStatusPredicate predicate = new RoleStatusPredicate(Arrays.asList("buyer"), Collections.emptyList());
 
         // TODO: Replace with actual test assertion
@@ -27,7 +26,7 @@ class RoleStatusPredicateTest {
     void test_matchesStatusOnly_returnsTrue() {
         // TODO: Implement test once RoleStatusPredicate.test() is implemented
         // TODO: Implement PersonBuilder.withStatus(String) method
-        Person person = new PersonBuilder() /* .withStatus("completed") */ .build();
+        Person person = new PersonBuilder()/* .withStatus("completed") */.build();
         RoleStatusPredicate predicate = new RoleStatusPredicate(Collections.emptyList(), Arrays.asList("completed"));
 
         // TODO: Replace with actual test assertion
@@ -38,7 +37,7 @@ class RoleStatusPredicateTest {
     void test_matchesRoleAndStatus_returnsTrue() {
         // TODO: Implement test once RoleStatusPredicate.test() is implemented
         // TODO: Implement PersonBuilder.withRole(String) and withStatus(String)
-        Person person = new PersonBuilder() /* .withRole("buyer").withStatus("completed") */ .build();
+        Person person = new PersonBuilder()/* .withRole("buyer").withStatus("completed") */.build();
         RoleStatusPredicate predicate = new RoleStatusPredicate(Arrays.asList("buyer"), Arrays.asList("completed"));
 
         // TODO: Replace with actual test assertion
@@ -49,7 +48,7 @@ class RoleStatusPredicateTest {
     void test_emptyKeywords_matchesAll() {
         // TODO: Implement test once RoleStatusPredicate.test() is implemented
         // TODO: Implement PersonBuilder.withRole(String) and withStatus(String)
-        Person person = new PersonBuilder() /* .withRole("anyrole").withStatus("anystatus") */ .build();
+        Person person = new PersonBuilder()/* .withRole("anyrole").withStatus("anystatus") */.build();
         RoleStatusPredicate predicate = new RoleStatusPredicate(Collections.emptyList(), Collections.emptyList());
 
         // TODO: Replace with actual test assertion
