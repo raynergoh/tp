fc67d8124370da06029e7e28a78f9100f0e68f7e
lucius <luciusyeo18@gmail.com>
2025-10-15 20:28:03 +0800

    Fix non-appearance bug for role field

--- src/main/java/seedu/address/storage/JsonAdaptedPerson.java
+++ src/main/java/seedu/address/storage/JsonAdaptedPerson.java
@@ -39,7 +39,7 @@ class JsonAdaptedPerson {
     @JsonCreator
     public JsonAdaptedPerson(@JsonProperty("name") String name, @JsonProperty("phone") String phone,
         @JsonProperty("email") String email, @JsonProperty("address") String address,
-         @JsonProperty("role") List<JsonAdaptedRole> roles, @JsonProperty("tags") List<JsonAdaptedTag> tags) {
+         @JsonProperty("roles") List<JsonAdaptedRole> roles, @JsonProperty("tags") List<JsonAdaptedTag> tags) {
         this.name = name;
         this.phone = phone;
         this.email = email;
