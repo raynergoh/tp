19cbd2c9193cb0004e73f57eb0c75a82298b0944
rayner <raynergoh03@gmail.com>
2025-10-22 00:58:15 +0800

    Add testfiles for TagGroupCommand

---
+++ src/test/java/seedu/address/logic/parser/TagGroupCommandParserTest.java
@@ -0,0 +1,41 @@
+package seedu.address.logic.parser;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static seedu.address.testutil.Assert.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.logic.commands.TagGroupCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.tag.TagGroup;
+
+/**
+ * Unit tests for TagGroupCommandParser.
+ */
+public class TagGroupCommandParserTest {
+
+    private final TagGroupCommandParser parser = new TagGroupCommandParser();
+
+    @Test
+    public void parse_emptyArgs_returnsListCommand() throws Exception {
+        TagGroupCommand command = parser.parse("");
+        assertEquals(new TagGroupCommand(null), command);
+    }
+
+    @Test
+    public void parse_whitespaceOnlyArgs_returnsListCommand() throws Exception {
+        TagGroupCommand command = parser.parse("   ");
+        assertEquals(new TagGroupCommand(null), command);
+    }
+
+    @Test
+    public void parse_validTagGroupName_returnsCreateCommand() throws Exception {
+        TagGroupCommand command = parser.parse("housing");
+        assertEquals(new TagGroupCommand(new TagGroup("housing")), command);
+    }
+
+    @Test
+    public void parse_invalidTagGroupName_throwsParseException() {
+        assertThrows(ParseException.class, () -> parser.parse("!!invalidTag"));
+    }
+}
