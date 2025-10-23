92740ad2a76387b2c5e751b4da68ee60e6ac999f
raynergoh <127125782+raynergoh@users.noreply.github.com>
2025-10-23 23:50:42 +0800

    Clean up AddressBookParserTest by removing blank lines
    Removed unnecessary blank lines in AddressBookParserTest.

--- src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddressBookParserTest.java
@@ -99,8 +99,6 @@ public class AddressBookParserTest {
         assertTrue(parser.parseCommand(TagGroupCommand.COMMAND_WORD + " MyGroup") instanceof TagGroupCommand);
     }
 
-
-
     @Test
     public void parseCommand_unrecognisedInput_throwsParseException() {
         assertThrows(ParseException.class, String.format(MESSAGE_INVALID_COMMAND_FORMAT, HelpCommand.MESSAGE_USAGE), ()
